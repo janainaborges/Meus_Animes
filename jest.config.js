2f987b855c76e83ba0188d0f90002f8f8741db90
Janaina Borges <janainaborgesdr@gmail.com>
2023-06-23 13:11:46 -0300

    Feat: o primeiro

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+};
