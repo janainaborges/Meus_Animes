eb699e80f2f9f4d52ce5740f7dd4894a4021d66b
Janaina Borges <janainaborgesdr@gmail.com>
2023-06-27 11:12:48 -0300

    ajuste no layout

--- src/test/routes/routes.test.ts
+++ src/test/routes/routes.test.ts
@@ -1,28 +1,28 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
-import Routes from '../routes';
-import Home from '../pages/Home';
-import Favoritos from '../pages/Favorite';
+import React from "react";
+import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import Routes from "../../routes";
+import Home from "../../pages/Home";
+import Favoritos from "../../pages/Favorite";
 
 test('renders home page when path is "/"', () => {
   const { getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
+    <MemoryRouter initialEntries={["/"]}>
       <Routes />
     </MemoryRouter>
   );
 
-  const homePageElement = getByText('Home Page');
+  const homePageElement = getByText("Home Page");
   expect(homePageElement).toBeInTheDocument();
 });
 
 test('renders favoritos page when path is "/favoritos"', () => {
   const { getByText } = render(
-    <MemoryRouter initialEntries={['/favoritos']}>
+    <MemoryRouter initialEntries={["/favoritos"]}>
       <Routes />
     </MemoryRouter>
   );
 
-  const favoritosPageElement = getByText('Favoritos Page');
+  const favoritosPageElement = getByText("Favoritos Page");
   expect(favoritosPageElement).toBeInTheDocument();
 });
